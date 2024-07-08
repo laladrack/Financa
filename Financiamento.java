58cc9a389f8b354287f4aedd6abe9158771e13cc
Maria Marques <113133620+laladrack@users.noreply.github.com>
2024-07-08 13:23:40 -0300

    adaptacao else if

--- Financiamento.java
+++ Financiamento.java
@@ -1,23 +1,46 @@
 //Maria Julia Lourenço Marques
 
 public class Financiamento {
-    public double valorImovel;
-    public int prazoFinanciamento;
-    public double taxaJurosAnual;
-
-    double PagamentoMensal;
-    double PagamentoTotal;
+    private double valorImovel;
+    private int prazoFinanciamento;
+    private double taxaJurosAnual;
 
     public Financiamento(v, p, t){
-        this.valorImovel = v;
-        this.prazoFinanciamento = p;
-        this.taxaJurosAnual = t; 
+        this.setvalorImovel(v);
+        this.setprazoFinanciamento(p);
+        this.settaxaJurosAnual(t); 
     }
     public void CalculoMensal(){
-        PagamentoMensal = (valorImovel/(prazoFinanciamento*12))*(1 + (taxaJurosAnual/12));
-        return PagamentoMensal
+        double PagamentoMensal = (valorImovel/(prazoFinanciamento*12))*(1 + (taxaJurosAnual/12));
+        return PagamentoMensal;
     }
     public void CalculoTotal(){
-        PagamentoTotal = PagamentoMensal*prazoFinanciamento*12;
+        double PagamentoTotal = PagamentoMensal*prazoFinanciamento*12;
+        return PagamentoTotal;
+    }
+    public void Status(){
+        System.out.println("O valor do imóvel é " + getvalorImovel);
+        System.out.println("O prazo do financiamento é " + getprazoFinanciamento);
+        System.out.println("A taxa de juros do imóvel é " + gettaxaJurosAnual);
+    }   
+
+
+    public void setvalorImovel(double valorImovel){
+        this.valorImovel = valorImovel;
+    }
+    public double getvalorImovel(){
+        return this.valorImovel;
+    }
+    public void setprazoFinanciamento(int prazoFinanciamento){
+        this.prazoFinanciamento = prazoFinanciamento;
+    }
+    public int getprazoFinanciamento(){
+        return this.prazoFinanciamento;
+    }
+    public void settaxaJurosAnual(double taxaJurosAnual){
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+    public double gettaxaJurosAnual(){
+        return this.taxaJurosAnual;
     }
 }
--- InterfaceUsuario.java
+++ InterfaceUsuario.java
@@ -5,18 +5,43 @@ public class InterfaceUsuario {
     void valor(){
         Scanner scanValor = new Scanner(System.in);
         System.out.println("Digite o valor do imóvel.");
-        String valorImovel = scanValor.nextLine();
-        System.out.println("O valor do imóvel é " + valorImovel);}
+        String input = scanValor.nextLine();
+        try{
+            double valorImovel = Double.parseDouble(input);
+
+            if (valorImovel() < 0){
+                System.out.println("Erro! Valor menor que zero.");
+            }
+            else{
+                System.out.println("O valor do imóvel é " + valorImovel);}
+            }
+
     void prazo(){
         Scanner scanPrazo = new Scanner(System.in);
         System.out.println("Digite o prazo do financiamento em anos.");
-        String prazoFinanciamento = scanPrazo.nextLine();
-        System.out.println("O prazo de pagamento do imóvel é " + prazoFinanciamento);
-    }
+        String input = scanPrazo.nextLine();
+        try{
+            int prazoFinanciamento = Integer.parseInt(input);
+
+            if (prazoFinanciamento < 0){
+                System.out.println("Erro! Valor menor que zero.");
+            }
+            else{
+                System.out.println("O prazo de pagamento do imóvel é " + prazoFinanciamento)}
+            }
+        }
     void taxa(){
         Scanner scanTaxa = new Scanner(System.in);
         System.out.println("Digite a taxa de juros anual.");
-        String taxaJurosAnual = scanTaxa.nextLine();
-        System.out.println("A taxa de juros do imóvel é " + taxaJurosAnual);
+        String input = scanTaxa.nextLine();
+        try{
+            double taxaJurosAnual = Double.parseDouble(input);
+
+            if (taxaJurosAnual() < 0){
+                System.out.println("Erro! Valor menor que zero.");
+            }
+            else{
+                System.out.println("A taxa de juros do imóvel é " + taxaJurosAnual);}
+            }
     }
 }
